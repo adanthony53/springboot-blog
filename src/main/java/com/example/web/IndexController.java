445336b4d0901e2e78a2b78f46f3907811ab73f1
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-09 16:50:48 -0700

    Added LogAspect

--- src/main/java/com/example/web/IndexController.java
+++ src/main/java/com/example/web/IndexController.java
@@ -3,17 +3,20 @@ package com.example.web;
 import com.example.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        // int i = 9/0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("blog is null");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable("id") Integer id, @PathVariable("name") String name) {
+//        int i = 9/0;
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFoundException("blog is null");
+//        }
+
+        System.out.println("id: " + id + ", name: " + name);
         return "index";
     }
 }
