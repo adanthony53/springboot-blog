89552aae1782f04930210da2383e3c91db6d8f23
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-10 14:13:44 -0700

    Added user login logout functions

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.dao.UserRepository;
 import com.example.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,7 +14,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        String encodedPassword = DigestUtils.md5DigestAsHex(password.getBytes());
+        User user = userRepository.findByUsernameAndPassword(username, encodedPassword);
         return user;
     }
 }
