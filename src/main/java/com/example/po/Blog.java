e2d467222544ce5719a67ff8906de4224a22423c
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-10 10:49:03 -0700

    Added po Objects

---
+++ src/main/java/com/example/po/Blog.java
@@ -0,0 +1,47 @@
+package com.example.po;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "t_blog")
+public class Blog {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+    private String firstPicture;
+    private String flag;
+    private Integer views;
+    private boolean donate;
+    private boolean shareStatement;
+    private boolean commentAllow;
+    private boolean publish;
+    private boolean recommend;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    @ManyToOne
+    private Type type;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST})
+    private List<Tag> tags;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "blog")
+    private List<Comment> comments = new ArrayList<>();
+}
