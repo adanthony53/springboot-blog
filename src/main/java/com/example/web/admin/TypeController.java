1f50f6128d6cd88545e16f0989e8fe571991a5a1
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-10 20:31:20 -0700

    Added admin Type management features

---
+++ src/main/java/com/example/web/admin/TypeController.java
@@ -0,0 +1,88 @@
+package com.example.web.admin;
+
+import com.example.po.Type;
+import com.example.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/admin")
+public class TypeController {
+
+    @Autowired
+    private TypeService typeService;
+
+    @GetMapping("/types")
+    public String list(@PageableDefault(size=10, sort = {"id"}, direction = Sort.Direction.ASC)
+                                   Pageable pageable, Model model) {
+        model.addAttribute("page", typeService.listType(pageable));
+        return "admin/types";
+    }
+
+    @GetMapping("/types/input")
+    public String input(Model model) {
+        model.addAttribute("type", new Type());
+        return "admin/types-input";
+    }
+
+    @GetMapping("/types/{id}/input")
+    public String editInput(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("type", typeService.getType(id));
+        return "admin/types-input";
+    }
+
+    @GetMapping("/types/{id}/delete")
+    public String delete(@PathVariable("id") Long id, RedirectAttributes attributes) {
+        typeService.deleteType(id);
+        attributes.addFlashAttribute("message", "Deleting type succeed");
+        return "redirect:/admin/types";
+    }
+
+    @PostMapping("/types")
+    public String post(@Valid Type type, BindingResult result, RedirectAttributes attributes) {
+        Type duplicate = typeService.getTypeByName(type.getName());
+        if (duplicate != null) {
+            result.rejectValue("name", "nameError", "Type name already exists");
+        }
+        if (result.hasErrors()) {
+            return "admin/types-input";
+        }
+        Type t = typeService.saveType(type);
+        if (t == null) {
+            attributes.addFlashAttribute("message", "Adding new type failed");
+        } else {
+            attributes.addFlashAttribute("message", "Adding new type succeed");
+        }
+        return "redirect:/admin/types";
+    }
+
+    @PostMapping("types/{id}")
+    public String editPost(@Valid Type type, BindingResult result, @PathVariable("id") Long id, RedirectAttributes attributes) {
+        Type duplicate = typeService.getTypeByName(type.getName());
+        if (duplicate != null) {
+            result.rejectValue("name", "nameError", "Type name already exists");
+        }
+        if (result.hasErrors()) {
+            return "admin/types-input";
+        }
+        Type t = typeService.updateType(id, type);
+        if (t == null) {
+            attributes.addFlashAttribute("message", "Updating new type failed");
+        } else {
+            attributes.addFlashAttribute("message", "Updating new type succeed");
+        }
+        return "redirect:/admin/types";
+    }
+}
