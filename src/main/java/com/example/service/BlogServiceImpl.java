12d223dc74fe4ce6d9a33fdb0328de048fd9dcc9
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-12 12:42:16 -0700

    Added search features

--- src/main/java/com/example/service/BlogServiceImpl.java
+++ src/main/java/com/example/service/BlogServiceImpl.java
@@ -65,6 +65,11 @@ public class BlogServiceImpl implements BlogService {
     }
 
     @Override
+    public Page<Blog> listBlog(String query, Pageable pageable) {
+        return blogRepository.findByQuery("%"+query+"%", pageable);
+    }
+
+    @Override
     public List<Blog> listRecommendBlogTop(Integer size) {
         return blogRepository.findTop(PageRequest.of(0, size, Sort.by(Sort.Direction.DESC, "updateTime")));
     }
